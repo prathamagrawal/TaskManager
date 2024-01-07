1cd678af367939f35ccdb1fdd1fc7888f6100339
Pratham Agrawal <prathamagrawal1205@gmail.com>
2024-01-07 17:50:02 +0000

    Added DockerFile

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for backend project.
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include 
-from rest_framework import routers
+from django.urls import path, include
 from todo import views
+from rest_framework import routers
 
-router=routers.DefaultRouter()
-router.register(r'tasks',views.TodoView,'task')
+router = routers.DefaultRouter()
+router.register(r'tasks', views.TodoView, 'task')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include(router.urls))
+    path('api/', include(router.urls))
 ]
